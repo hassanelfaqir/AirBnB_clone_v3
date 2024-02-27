be814a79540eb727300d1f2fd5b0405e98f4b50d
hassanelfaqir <hassanelfaqir@Linux.myguest.virtualbox.org>
2024-02-27 12:41:14 +0100

    AirBnB_clone_v3

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+""" holds class Amenity"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+
+
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
